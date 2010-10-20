b9fed9a553ea3a0b8e541b5c1cd1f043d192aa1d
Alexander Striffeler <alexanders@bluewin.ch>
2010-10-20 17:20:32 +0200

    some minor changes in the bootstrap-job -> for file-transfer only=)

--- qa/app/jobs/BootStrap.java
+++ qa/app/jobs/BootStrap.java
@@ -1,7 +1,10 @@
 package jobs;
 
+import java.util.ArrayList;
 import java.util.Random;
 
+import models.Answer;
+import models.DbManager;
 import models.Question;
 import models.User;
 
@@ -16,6 +19,7 @@ public class BootStrap extends Job {
 
 	private static final Log log = LogFactory.getLog(BootStrap.class);
 	private static Random random = new Random();
+	private static DbManager manager = DbManager.getInstance();
 
 	// boolean up= (random.nextInt(2) == 0 ? true : false);
 	//
@@ -36,26 +40,42 @@ public class BootStrap extends Job {
 		// create some users
 		for (int i = 1; i <= users; i++) {
 			User u = new User("user-" + i, "user-" + i + "@ese.ch", "user-" + i);
-			// 5 questions per user
-			for (int j = 1; j <= questionsPerUser; j++) {
-				Question q = new Question("question " + j, u);
-			}
+			// // 5 questions per user
+			// for (int j = 1; j <= questionsPerUser; j++) {
+			// Question q = new Question("question " + j, u);
+			// }
 		}
 
-		// Collection<Question> questions = Question.findAll();
-		//
-		// //an answer for every second question, from a random user
-		// int i = 0;
-		// for(Question q : questions){
-		//
-		// if( i++ % 2 == 0){
-		// User u = User.findById(random.nextInt(users)+1);
-		// Answer a = new Answer(u, "Answer to question "+q.getId());
-		// q.addAnswer(a);
-		// randomVote(a, u);
-		// randomVote(q, u);
-		// }
-		// }
+		ArrayList<Question> questions = manager.getQuestions();
+
+		// add questions with tags
+		Question q1 = new Question("How small is the fish?", manager
+				.getUserByName("user-1"));
+		q1.addTags("fish size");
+		q1.vote("1");
+		new Answer("as big as the fisherman isn't yet", manager
+				.getUserByName("user-2"), q1);
+
+		Question q2 = new Question(
+				"How many roads must a man walk down before you can call him a man?",
+				manager.getUserByName("user-5"));
+		q2.addTags("man road");
+		new Answer("The answer my friend is blowin in the wind", manager
+				.getUserByName("user-3"), q2);
+		q1.vote("1");
+		q1.vote("1");
+
+		// an answer for every second question from the first user
+		int i = 0;
+		for (Question q : questions) {
+
+			if (i++ % 2 == 0) {
+				User u = manager.getUsers().get(0);
+				Answer a = new Answer("answer " + i, u, q);
+			} else {
+				q.vote("-1");
+			}
+		}
 
 	}
 
