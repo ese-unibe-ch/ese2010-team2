7075b0ee2f7701f94ba220264e2762d0c6a56adf
Bjoern Mosler <brelsom@gmx.net>
2010-11-14 16:35:35 +0100

    Improve UI according to customer's wishes. Disclaimer: It's not yet fully functional.
    - recent questions and search results now appear in the right column.
    - change tagbox placement in short_post such that it looks ok if there
      are multiple tags.
    - add a home icon.

--- qa/app/controllers/Application.java
+++ qa/app/controllers/Application.java
@@ -1,30 +1,16 @@
 package controllers;
 
-import java.io.File;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import models.Answer;
 import models.DbManager;
-import models.Post;
 import models.Question;
 import models.SearchManager;
 import models.SearchResult;
 import models.User;
-
-import org.apache.commons.io.IOUtils;
-
-import play.Play;
-import play.cache.Cache;
 import play.mvc.Controller;
-import annotations.Unused;
 
 /**
  * This Controller manages some general actions.
@@ -47,7 +33,6 @@ public class Application extends Controller {
 			render(message, userName, score);
 		} else {
 			ArrayList<Question> questions = manager.getQuestionsSortedByScore();
-			Cache.set("questions", questions);
 			render(questions, userName, isChanged);
 		}
 	}
--- qa/app/controllers/DisplayQuestionController.java
+++ qa/app/controllers/DisplayQuestionController.java
@@ -1,27 +1,13 @@
 package controllers;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import models.Answer;
 import models.DbManager;
 import models.Post;
 import models.Question;
-import models.SearchManager;
-import models.SearchResult;
-import models.User;
-
-import org.apache.commons.io.IOUtils;
-
-import play.Play;
-import play.cache.Cache;
 import play.mvc.Controller;
 import annotations.Unused;
 
